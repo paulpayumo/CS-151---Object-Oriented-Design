e0559d901c12a2b7f81bd4e76744a5090143043a
paulpayumo <60272942+paulpayumo@users.noreply.github.com>
2020-01-26 17:05:23 -0800

    Create StudentTest.java

---
+++ Assignment2/Ex1/StudentTest.java
@@ -0,0 +1,10 @@
+class StudentTest
+{
+
+  public static void main(String[] args)
+  {
+    Student john = new Student(John, Smith, 20, 3.6, Computer Science, School of Computer Science);
+    john.printSchedule();
+  } 
+  
+}
