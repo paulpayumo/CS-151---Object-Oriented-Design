c5e9ed4110a2f0a4aae0d91cd5aa446fc834104d
paulpayumo <60272942+paulpayumo@users.noreply.github.com>
2020-02-10 20:30:08 -0800

    Update Cat.java

--- Assignment2/Ex5/Cat.java
+++ Assignment2/Ex5/Cat.java
@@ -1,1 +1,40 @@
+public class Cat extends Animal implements Domesticated{
+	private String temperament;
+	
+	public Cat(String temperament, String type, String name, int age, String gender, String location, int speed)
+	{
+		super(type, name, age, gender, location, speed);
+		this.temperament = temperament;
+	}
+	
+	public void walk()
+	{
+		sop("I am walking.");
+	}
+	
+	public void greetHuman()
+	{
+		sop("Do cats really greet humans though?");
+	}
+	
+	public void scratch()
+	{
+		sop("IMMA SCRATCH YOU");
+	}
+	
+	public String getTemperament()
+	{
+		return temperament;
+	}
+	
+	public void setTemperament(String newTemperament)
+	{
+		this.temperament = newTemperament;
+	}
+	public String toString()
+	{
+		String myString = super.toString();
+		return myString + "I am also " + this.getTemperament() + ".";
+	}
+}
 
