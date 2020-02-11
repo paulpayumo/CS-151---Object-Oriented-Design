833f9d2eb94145724ce4255b1b1160635180bb17
paulpayumo <60272942+paulpayumo@users.noreply.github.com>
2020-02-10 20:31:34 -0800

    Create Whale.java

---
+++ Assignment2/Ex5/Whale.java
@@ -0,0 +1,12 @@
+public class Whale extends Animal
+{
+	public Whale(String type, String name, int age, String gender, String location, int speed)
+	{
+		super(type, name, age, gender, location, speed);
+	}
+	
+	public void swim()
+	{
+		sop("Just keep swimming, Just keep Swimming, Just keep swimming swimming swimming.");
+	}
+}
