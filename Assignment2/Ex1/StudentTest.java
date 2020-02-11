b8560aa69ef748346fd8191e70125bf3e31bbacd
paulpayumo <60272942+paulpayumo@users.noreply.github.com>
2020-02-10 20:21:08 -0800

    Update StudentTest.java

--- Assignment2/Ex1/StudentTest.java
+++ Assignment2/Ex1/StudentTest.java
@@ -4,7 +4,7 @@ class StudentTest
   public static void main(String[] args)
   {
     Student john = new Student("John", "Smith", 20, 3.6, "Computer Science", "School of Computer Science");
+    john.getInfo();
     john.printSchedule();
   } 
   
-}
