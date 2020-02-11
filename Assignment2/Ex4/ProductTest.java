c837e0d2a18d76928a3de22bce37653a44bdf96f
paulpayumo <60272942+paulpayumo@users.noreply.github.com>
2020-02-10 20:27:58 -0800

    Update ProductTest.java

--- Assignment2/Ex4/ProductTest.java
+++ Assignment2/Ex4/ProductTest.java
@@ -1,1 +1,8 @@
-
+public class ProductTest {
+	public static void main(String[] args)
+	{
+		Product practice = new Product("prac", "this is just my practice description. Let's just say I make you happy", 2, 18);
+		//practice.setName("fail");
+		System.out.println(practice.toString());
+	}
+}
