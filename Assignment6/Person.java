abaef21d27664435c8f7cc41db898060fbb6d52e
paulpayumo <60272942+paulpayumo@users.noreply.github.com>
2020-03-16 13:04:48 -0700

    Create Person.java

---
+++ Assignment6/Person.java
@@ -0,0 +1,41 @@
+
+public class Person 
+{
+	private String first;
+	private String last;
+	private int age;
+	
+	public Person() {}
+	
+	public void setFirst(String first)
+	{
+		this.first = first;
+	}
+	
+	public void setLast(String last)
+	{
+		this.last = last;
+	}
+	
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	
+	public String getFirst()
+	{
+		return this.first;
+	}
+	
+	public String getLast()
+	{
+		return this.last;
+	}
+	
+	public int getAge()
+	{
+		return this.age;
+	}
+}
+
+
