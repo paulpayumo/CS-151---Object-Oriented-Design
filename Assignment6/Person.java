883114f04446b1a8e61df51a957b538d93d3ffbb
paulpayumo <60272942+paulpayumo@users.noreply.github.com>
2020-03-16 13:37:04 -0700

    Update Person.java

--- Assignment6/Person.java
+++ Assignment6/Person.java
@@ -1,5 +1,6 @@
+import java.util.Comparator;
 
-public class Person 
+public class Person implements Comparable<Person>
 {
 	private String first;
 	private String last;
@@ -35,6 +36,35 @@ public class Person
 	public int getAge()
 	{
 		return this.age;
+	}
+	
+	public static final Comparator<Person> FirstComp = new Comparator<Person>() 
+	{
+		public int compare(Person first, Person second)
+		{
+			return first.getFirst().compareTo(second.getFirst());
+		}
+	};
+	
+	public static final Comparator<Person> LastComp = new Comparator<Person>() 
+	{
+		public int compare(Person first, Person second)
+		{
+			return first.getLast().length() - second.getLast().length();
+		}
+	};
+	
+	public static final Comparator<Person> AgeComp = new Comparator<Person>() 
+	{
+		public int compare(Person first, Person second)
+		{
+			return first.getAge() - second.getAge();
+		}
+	};
+
+	@Override
+	public int compareTo(Person o) {
+		return this.age - o.age;
 	}
 }
 
